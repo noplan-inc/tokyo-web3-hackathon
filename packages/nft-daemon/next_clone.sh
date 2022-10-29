82b82c360e010006da5c00fd34dc9482e6e9750b
serinuntius <wasab1gj@gmail.com>
2022-10-29 12:48:37 +0100

    add clone script

---
+++ packages/nft-daemon/next_clone.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# bashのスイッチ
+set -euC
+WORK_DIR="tmp/site";
+
+DOMAIN=$1;
+
+cd $WORK_DIR;
+
+echo "mkdir $DOMAIN";
+mkdir $DOMAIN;
+
+echo "cd " $DOMAIN;
+cd $DOMAIN;
+
+echo "cp frontend";
+cp -r /Users/serinuntius/src/github.com/noplan-inc/tokyo-web3-hackathon/packages/frontend/ .
+
+echo "yarn install...";
+yarn install;
+echo "yarn build...";
+
+yarn build;
+
