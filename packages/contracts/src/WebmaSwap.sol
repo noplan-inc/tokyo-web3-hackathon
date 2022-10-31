d502e19606ffea718c0292a53debb263bfed3165
SomaSekimoto <0421.soma@gmail.com>
2022-11-01 03:42:34 +0900

    (contract)create: WebmaSwap contract

---
+++ packages/contracts/src/WebmaSwap.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+
+
+contract WebmaSwap {
+    address webmaTokenAddress = 0x0000000000000000000000000000000000000000;
+    IERC721 webmaTokenContract = IERC721(webmaTokenAddress);
+
+    
+    struct Swap {
+        address owner;
+        uint256 tokenId;
+        address erc20;
+        uint256 price;
+        bool locked;
+    }
+
+    mapping(uint256 => Swap) public swaps;
+
+    event Open(uint256 tokenId, address erc20, uint256 price);
+    event Close(uint256 tokenId);
+    event Fulfill(uint256 tokenId, address erc20, uint256 price, address newOwner);
+
+    constructor(){}
+
+
+    modifier isOwner(uint256 tokenId){
+        require(msg.sender == swaps[tokenId].owner , "sender is not NFT owner.");
+        _;
+    }
+
+
+    function open(uint256 tokenId, address erc20, uint256 price) public isOwner(tokenId)  {
+        Swap memory newSwap = Swap(msg.sender, tokenId, erc20, price, false);
+        swaps[tokenId] = newSwap;
+        webmaTokenContract.approve(address(this), tokenId);
+        emit Open(tokenId, erc20,  price);
+    }
+
+    function close(uint256 tokenId) public isOwner(tokenId) {
+        delete swaps[tokenId];
+        emit Close(tokenId);
+    }
+
+    function fulfill(uint256 tokenId, address newOwner) public{
+        Swap memory swap = swaps[tokenId];
+        webmaTokenContract.transferFrom(swap.owner, newOwner, tokenId);
+        delete swaps[tokenId];
+        emit Fulfill(tokenId, swap.erc20, swap.price, newOwner);
+    }
+}
+
--- packages/contracts/src/WebmaToken.sol
+++ packages/contracts/src/WebmaToken.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.14;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract WebmaToken is ERC721, Ownable {
+
+contract WebmaToken is ERC721 {
     constructor() ERC721("WebmaToken", "WMT") {}
 }
