a1c5bc955298aea33774005be0a513ac7eda194b
SomaSekimoto <0421.soma@gmail.com>
2022-11-01 18:13:30 +0900

    (contracts)fix: erc20 transfer, reentrant modifier & constructor

--- packages/contracts/src/WebmaSwap.sol
+++ packages/contracts/src/WebmaSwap.sol
@@ -2,13 +2,14 @@
 pragma solidity ^0.8.14;
 
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/IERC721.sol";
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "../lib/openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
 
-contract WebmaSwap {
-    address webmaTokenAddress = 0x0000000000000000000000000000000000000000;
-    IERC721 webmaTokenContract = IERC721(webmaTokenAddress);
+contract WebmaSwap is ReentrancyGuard {
+    address public webmaTokenAddress;
+    IERC721 private webmaTokenContract;
 
-    
     struct Swap {
         address owner;
         uint256 tokenId;
@@ -23,37 +24,32 @@ contract WebmaSwap {
     event Close(uint256 tokenId);
     event Fulfill(uint256 tokenId, address erc20, uint256 price, address newOwner);
 
-    constructor(){}
-
+    constructor(address wmtAddress) {
+        webmaTokenAddress = wmtAddress;
+        webmaTokenContract = IERC721(webmaTokenAddress);
+    }
 
     modifier isOwner(uint256 tokenId){
         require(msg.sender == swaps[tokenId].owner , "sender is not NFT owner.");
         _;
     }
 
-    modifier noReentrant(uint256 tokenId) {
-        require(!swaps[tokenId].locked, "No re-entrancy");
-        swaps[tokenId].locked = true;
-        _;
-        swaps[tokenId].locked = false;
-    }
-
-
     function open(uint256 tokenId, address erc20, uint256 price) public isOwner(tokenId)  {
         Swap memory newSwap = Swap(msg.sender, tokenId, erc20, price, false);
         swaps[tokenId] = newSwap;
-        webmaTokenContract.approve(address(this), tokenId);
+        // webmaTokenContract.approve(address(this), tokenId);
         emit Open(tokenId, erc20,  price);
     }
 
-    function close(uint256 tokenId) public isOwner(tokenId) noReentrant(tokenId) {
+    function close(uint256 tokenId) public isOwner(tokenId) {
         delete swaps[tokenId];
         emit Close(tokenId);
     }
 
-    function fulfill(uint256 tokenId, address newOwner) public noReentrant(tokenId){
+    function fulfill(uint256 tokenId, address newOwner) public nonReentrant(){
         Swap memory swap = swaps[tokenId];
         webmaTokenContract.transferFrom(swap.owner, newOwner, tokenId);
+        IERC20(swap.erc20).transfer(swap.owner, swap.price);
         delete swaps[tokenId];
         emit Fulfill(tokenId, swap.erc20, swap.price, newOwner);
     }
