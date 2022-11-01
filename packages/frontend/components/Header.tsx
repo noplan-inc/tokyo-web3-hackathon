69cc77365bd8c42e88cf4b7b150d3edf47d940c3
折田幸夫 <3204704@gmail.com>
2022-11-02 00:54:02 +0900

    feat setting

---
+++ packages/frontend/components/Header.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Heading, Button, Flex, Box, Spacer } from "@chakra-ui/react";
+
+type Props = {};
+
+export const Header: React.FC<Props> = () => {
+  return (
+    <Flex minWidth="max-content" alignItems="center" gap="2">
+      <Box p="2">
+        <Heading as="h1" size="2xl">
+          LSAT App⚡️
+        </Heading>
+      </Box>
+      <Spacer />
+      <Button colorScheme="teal" size="lg">
+        Connect
+      </Button>
+    </Flex>
+  );
+};
--- packages/frontend/pages/[token_id]/setting.tsx
+++ packages/frontend/pages/[token_id]/setting.tsx
@@ -1,17 +1,67 @@
 import type { NextPage } from "next";
-import { Heading } from "@chakra-ui/react";
-import { Button } from "@chakra-ui/react";
+import { Heading, Button, Flex, Box, Spacer } from "@chakra-ui/react";
+import { ChevronLeftIcon } from "@chakra-ui/icons";
+import { Header } from "../../components/Header";
 
 // _____________________________________________________________________________
 //
 const Page: NextPage = () => {
   return (
-    <div>
-      <Heading>Setting</Heading>
-      <Button colorScheme="teal" size="lg">
-        Connect
+    <Box p="12">
+      <Header />
+      <Button>
+        <ChevronLeftIcon w={6} h={6} />
       </Button>
-    </div>
+
+      <Heading as="h2" size="xl" mt="24px">
+        item for sale
+      </Heading>
+      {/* contents */}
+      <Box>
+        <Heading as="h3" size="md" mt="12px">
+          Type
+        </Heading>
+        <Box>Fixed Price💰</Box>
+      </Box>
+
+      <Box>
+        <Heading as="h3" size="md" mt="12px">
+          Type
+        </Heading>
+        <Box>Fixed Price💰</Box>
+      </Box>
+
+      <Box>
+        <Heading as="h3" size="md" mt="12px">
+          Price
+        </Heading>
+        <Box>input</Box>
+      </Box>
+
+      <Box>
+        <Heading as="h3" size="md" mt="12px">
+          Whether or not to sell
+        </Heading>
+        <Box>toggle</Box>
+      </Box>
+
+      <Box>
+        <Heading as="h3" size="md" mt="12px">
+          Preview
+        </Heading>
+        <Box>画像</Box>
+      </Box>
+
+      <Box>
+        <Heading as="h3" size="md" mt="12px">
+          Description
+        </Heading>
+        <Box>text area</Box>
+      </Box>
+      <Button colorScheme="teal" size="lg">
+        Completed
+      </Button>
+    </Box>
   );
 };
 // _____________________________________________________________________________
