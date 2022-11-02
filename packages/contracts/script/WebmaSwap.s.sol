6fc27b311453a95448d7e6121142f84aa51e7372
SomaSekimoto <0421.soma@gmail.com>
2022-11-02 18:23:00 +0900

    (contracts)add: deploy script

---
+++ packages/contracts/script/WebmaSwap.s.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "../lib/forge-std/src/Script.sol";
+import "../src/WebmaSwap.sol";
+import "../src/WebmaToken.sol";
+
+contract MyScript is Script {
+    function run() external {
+        vm.startBroadcast();
+
+        WebmaToken webmaToken = new WebmaToken();
+        new WebmaSwap(address(webmaToken));
+        
+        vm.stopBroadcast();
+    }
+}
